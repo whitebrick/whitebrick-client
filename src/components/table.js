12b3b72de922cc23062494bf686aad017760fed7
harshithpabbati <pabbatiharshith@gmail.com>
2021-05-05 00:42:05 +0530

    allow users to select the table and render accordingly

--- src/components/table.js
+++ src/components/table.js
@@ -1,37 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AgGridColumn, AgGridReact } from 'ag-grid-react'
-import { useQuery } from 'graphql-hooks'
+import { useManualQuery, useQuery } from 'graphql-hooks'
 
-const GET_TABLE_DATA = `{
-  donnasdvd_customer {
-    first_name
-    last_name
-    active
-    activebool
-    address_id
-    create_date
-    customer_id
-    email
-    last_update
-    store_id
+import graphQLFetch from '../utils/GraphQLFetch';
+
+const TABLES_QUERY = `
+{
+  __schema{
+    queryType{
+      fields{
+        name
+      }
+    }
   }
 }
 `;
 
+
+const GET_TABLE_FIELDS = `query ($name: String!){
+  __type(name: $name) {
+    name
+    fields {
+      name
+    }
+  }
+}`;
+
 const Table = () => {
-  const { loading, error, data } = useQuery(GET_TABLE_DATA);
+  const {loading, error, data} = useQuery(TABLES_QUERY);
+
+  const [table, setTable] = useState('');
+  const [tableData, setData] = useState([]);
+
+  const [fetchQueryFields] = useManualQuery(GET_TABLE_FIELDS);
+
+  const generateQuery = ({ name, fields }) => {
+    let queryFields = '';
+    fields.map(field => {
+      queryFields += ' '+ field.name;
+    });
+    return `{ ${name} { ${queryFields} } }`;
+  };
+
+  useEffect(() => {
+    const handleTableChange = async () => {
+      if (table !== '') {
+        const { data } = await fetchQueryFields({ variables: { name: table }})
+        const query = generateQuery(data['__type']);
+        const fetchData = async () => await graphQLFetch({ query });
+        fetchData().then(({ data }) => {
+          setData(data[table]);
+        });
+      }
+    };
+    handleTableChange();
+  }, [table]);
 
   if (loading) return 'Loading...'
   if (error) return 'Something Bad Happened'
 
   return (
     <div className="ag-theme-alpine" style={{ height: '100vh' }}>
-      <AgGridReact
-        rowData={data.donnasdvd_customer}>
-        {Object.keys(data.donnasdvd_customer[0]).map(key => (
-          <AgGridColumn field={key} />
+      <select value={table} onChange={e => setTable(e.target.value)}>
+        <option defaultChecked>Select a table</option>
+        {data['__schema'].queryType.fields.map(field => (
+          <option value={field.name}>{field.name}</option>
         ))}
-      </AgGridReact>
+      </select>
+      {table !== '' && tableData.length > 0 ?
+        <AgGridReact
+          rowData={tableData}>
+          {Object.keys(tableData[0]).map(key => (
+            <AgGridColumn field={key} />
+          ))}
+        </AgGridReact>
+        : <p>Please select a table to render</p>
+      }
     </div>
   )
 };
---
+++ src/utils/GraphQLFetch.js
@@ -0,0 +1,34 @@
+import fetch from 'isomorphic-fetch';
+
+export default ({ query, variables }) => {
+  const body = {
+    query,
+    variables,
+  };
+
+  const apiConfig = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-hasura-admin-secret': process.env.GATSBY_HASURA_GRAPHQL_ADMIN_SECRET
+    },
+    body: JSON.stringify(body),
+  };
+
+  return fetch(process.env.GATSBY_HASURA_GRAPHQL_URL, apiConfig).then(function (response) {
+    const contentType = response.headers.get('content-type');
+    if (response.ok) {
+      if (contentType && contentType.indexOf('application/json') !== -1) {
+        return response.json().then((json) => json);
+      }
+      if (contentType && contentType.indexOf('text') !== -1) {
+        return response.text().then((text) => text);
+      }
+      return response;
+    }
+    console.error(
+      `Response status ${response.status} during dataFetch for url ${response.url}.`
+    );
+    throw response;
+  });
+};
