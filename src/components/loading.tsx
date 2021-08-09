e09835c5d856870e0aa4847cb112f5cf2c83a9e6
sevagenv <sevagenv@gmail.com>
2021-08-09 12:04:44 +0400

    fix: Add a basic loading animation
    Signed-off-by: sevagenv <sevagenv@gmail.com>

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import '../styles/loading.css'
 
 const Loading = () => {
   return (
     <div className="d-flex align-items-center min-vh-100">
       <div className="container text-center">
-        <h3>Loading...</h3>
+        <div className="ripple">
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
