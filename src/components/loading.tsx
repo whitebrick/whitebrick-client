fea2780516223abd6870ad2b9c3f45f5d43f5a4f
harshithpabbati <pabbatiharshith@gmail.com>
2021-08-10 13:22:03 +0530

    chore: change the loading page ripple color

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -6,8 +6,8 @@ const Loading = () => {
     <div className="d-flex align-items-center min-vh-100">
       <div className="container text-center">
         <div className="ripple">
-          <div></div>
-          <div></div>
+          <div />
+          <div />
         </div>
       </div>
     </div>
