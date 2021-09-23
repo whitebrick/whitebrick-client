1cd530a1aaab56eaab3c77b69ea3b60eafb4f0f1
harshithpabbati <pabbatiharshith@gmail.com>
2021-09-23 18:15:09 +0530

    fix: check for org and schema name and render the redirect page if required

--- src/pages/[organizationName]/[databaseName]/index.tsx
+++ src/pages/[organizationName]/[databaseName]/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Layout from '../../../components/layouts/layout';
 import SchemaLayout from '../../../components/layouts/schemaLayout';
+import Redirect from '../../../components/common/redirect';
 
 type OrgSchemaPropsType = {
   params: any;
 };
 
 const OrgSchema = ({ params }: OrgSchemaPropsType) => {
+  if (
+    params.organizationName === 'redirect' &&
+    params.databaseName === 'index.html'
+  )
+    return <Redirect />;
   return (
     <Layout>
       <SchemaLayout params={params} />
