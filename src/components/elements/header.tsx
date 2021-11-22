03933bbf6d6570920e149891fd758a626a969a8c
sevagenv <sevagenv@gmail.com>
2021-11-22 10:37:11 +0400

    chore: move menu from header for shadowing
    Signed-off-by: sevagenv <sevagenv@gmail.com>

--- src/components/elements/header.tsx
+++ src/components/elements/header.tsx
@@ -1,37 +1,10 @@
 import * as React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
 import { navigate } from 'gatsby';
-import {
-  Popover,
-  Menu,
-  Position,
-  IconButton,
-  Avatar,
-  LogOutIcon,
-  SettingsIcon,
-  PlusIcon,
-  PanelTableIcon,
-  DatabaseIcon,
-  ApplicationsIcon,
-  UserIcon,
-} from 'evergreen-ui';
-
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import HeaderMenu from './headerMenu';
 // @ts-ignore
 import WhitebrickLogo from '../../images/whitebrick-logo-square.svg';
-import { actions } from '../../state/actions';
-import { SchemaItemType } from '../../types';
 
-type HeaderPropsType = {
-  user: any;
-  schema: SchemaItemType;
-  actions: any;
-};
-
-const Header = ({ user, schema, actions }: HeaderPropsType) => {
-  const { logout } = useAuth0();
-
+const Header = () => {
   return (
     <header className="bg-white nav-shadow fixed-top">
       <nav className="navbar navbar-light">
@@ -45,90 +18,10 @@ const Header = ({ user, schema, actions }: HeaderPropsType) => {
             </span>
           </div>
         </span>
-        <div className="navbar-text d-flex">
-          <Popover
-            position={Position.BOTTOM_RIGHT}
-            content={
-              <Menu>
-                <Menu.Group>
-                  <Menu.Item
-                    icon={PanelTableIcon}
-                    onClick={() => {
-                      actions.setFormData({ schema });
-                      actions.setType('createTable');
-                      actions.setShow(true);
-                    }}>
-                    New Table
-                  </Menu.Item>
-                  <Menu.Item
-                    icon={DatabaseIcon}
-                    onClick={() => {
-                      actions.setFormData({});
-                      actions.setType('createDatabase');
-                      actions.setShow(true);
-                    }}>
-                    New Database
-                  </Menu.Item>
-                  <Menu.Item
-                    icon={ApplicationsIcon}
-                    onClick={() => {
-                      actions.setFormData({});
-                      actions.setType('createOrganization');
-                      actions.setShow(true);
-                    }}>
-                    New Organization
-                  </Menu.Item>
-                </Menu.Group>
-              </Menu>
-            }>
-            <IconButton
-              icon={PlusIcon}
-              marginRight={10}
-              appearance="minimal"
-              height={20}
-            />
-          </Popover>
-          <Popover
-            position={Position.BOTTOM_RIGHT}
-            content={
-              <Menu>
-                <Menu.Group>
-                  <Menu.Item
-                    icon={UserIcon}
-                    onClick={() => navigate('/profile')}>
-                    Profile
-                  </Menu.Item>
-                </Menu.Group>
-                <Menu.Group>
-                  <Menu.Item icon={SettingsIcon}>Settings</Menu.Item>
-                </Menu.Group>
-                <Menu.Divider />
-                <Menu.Group>
-                  <Menu.Item
-                    icon={LogOutIcon}
-                    onClick={() =>
-                      logout({ returnTo: window.location.origin })
-                    }>
-                    Log out
-                  </Menu.Item>
-                </Menu.Group>
-              </Menu>
-            }>
-            <Avatar src={user.picture} name={user.nickname} size={20} />
-          </Popover>
-        </div>
+        <HeaderMenu />
       </nav>
     </header>
   );
 };
 
-const mapStateToProps = state => ({
-  user: state.user,
-  schema: state.schema,
-});
-
-const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators(actions, dispatch),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default Header;
