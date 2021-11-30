7e259cda7d8b47bf89b812ce9e1a85b24339e8a6
sevagenv <sevagenv@gmail.com>
2021-11-30 13:33:47 +0400

    0.1.7
    Signed-off-by: sevagenv <sevagenv@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,18 +26,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/whitebrick-logo.svg`, // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`Shadows Into Light Two`],
